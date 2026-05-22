2f058fa407d843e974647b0cc4512382ba3d8eb8
CompactModel <yan.oleksuyk7@gmail.com>
2026-05-22 09:04:48 +0200

    Fix: symfony-cmd shim for composer scripts, add assets:install to startup

--- app/startup.sh
+++ app/startup.sh
@@ -2,6 +2,7 @@
 set -e
 
 php bin/console cache:clear --env=prod --no-debug
+php bin/console assets:install --env=prod
 php bin/console doctrine:migrations:migrate --no-interaction --env=prod
 
 exec php -S "0.0.0.0:${PORT:-8000}" -t public/ public/router.php
